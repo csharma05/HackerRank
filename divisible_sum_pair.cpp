04aa69f1b3dfd9dec501df91bd4aa338da7c0b8c
csharma05 <78192378+csharma05@users.noreply.github.com>
2022-11-30 18:47:45 +0530

    Create divisible_sum_pair.cpp

---
+++ divisible_sum_pair.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    int count =0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if((arr[i]+arr[j])%k==0 && i<j){
+                count++;
+            }
+        }
+    }
+    cout<<count;
+}
